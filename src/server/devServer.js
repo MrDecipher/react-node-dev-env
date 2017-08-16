81dbaa7854e52efc48de7e1b8a3c8867753fd404
Ishola Japheth <japhethish@gmail.com>
2017-08-16 16:30:59 +0100

    Removed Webpack

--- src/server/devServer.js
+++ src/server/devServer.js
@@ -1,22 +1,10 @@
 const express = require("express");
 const app = express();
 
-// ======================= Webpack Requirements ================================
-const webpack = require("webpack");
-const webpackConfig = require("../../webpack.config.js");
-const compiler = webpack(webpackConfig);
-const webpackMiddleware = require("webpack-dev-middleware");
-
 const path = require("path");
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "../production")));
-
-app.use(webpackMiddleware(compiler, {
-    noInfo: false,
-    publicPath: webpackConfig.output.publicPath,
-    stats: { colors: true }
-}));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../production/index.html"));
