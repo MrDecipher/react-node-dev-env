8cb6dd842ba3a5285151d132ba74a9f301c73838
Ishola Japheth <japhethish@gmail.com>
2017-11-21 09:47:22 +0000

    Updated sassCompiler.js

--- sassCompiler.js
+++ sassCompiler.js
@@ -1,3 +1,4 @@
+// SCSS compiler that uses the node API
 const sass = require("node-sass");
 const fs = require("fs");
 const chalk = require("chalk");
@@ -7,12 +8,16 @@ sass.render({
     outFile: "./src/production/css"
 }, (err, result) => {
     if(!err) {
-        console.log(chalk.magenta("No errors during sass styles compilation"));
+        console.log(chalk.magenta("Successfully accessed SCSS"));
         fs.writeFile("./src/production/css/styles.css", result.css, (err) => {
             if(!err) {
                 /*eslint-disable no-console*/
-                console.log(chalk.green.bold("Successfully built css file ;)"));
+                console.log(chalk.green.bold("Successfully compiled SCSS ;)"));
             }
         });
+    } else {
+        // If an error occurs here, then there is a compilation Error
+        // Check your SCSS code then
+        console.log(chalk.red.bold("Error accessing or compiling SCSS"))
     }
 });
