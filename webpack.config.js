3356569a50be5dc0d7a7be49c9aa3922bfc619a3
Ishola Japheth <japhethish@gmail.com>
2018-06-23 10:04:01 +0100

    Added SASS Compilation To Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
 const webpack = require("webpack");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: [ 
+        "./src/index.js",
+        "./src/styles/styles.scss"
+    ],
     output: {
-        path: path.resolve(__dirname, "./dist/js"),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, "./dist"),
+        filename: "./js/bundle.js"
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./css/styles.css",
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /(\.css|\.scss|\.sass)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
         ]
     }
